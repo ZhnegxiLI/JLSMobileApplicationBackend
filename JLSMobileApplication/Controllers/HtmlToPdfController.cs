79d2a824ab7750d077b0bed8a692486b2e25cf94
ZhengxiLI <57601687+ZhnegxiLI@users.noreply.github.com>
2020-06-26 14:56:01 +0200

    完成部分导出模板

---
+++ JLSMobileApplication/Controllers/HtmlToPdfController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using JLSDataAccess.Interfaces;
+using JLSMobileApplication.HtmlToPdf;
+using Magicodes.ExporterAndImporter.Pdf;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+
+namespace JLSMobileApplication.Controllers
+{
+    [Route("api/[controller]/{action}/{id?}")]
+    [ApiController]
+    public class HtmlToPdfController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public HtmlToPdfController(IOrderRepository order)
+        {
+            _orderRepository = order;
+        }
+
+        public async Task<ActionResult> ExportPdf()
+        {
+            /* File name */
+            string fileName = "Exports/" + DateTime.Now.Second.ToString()+"_Invoice.pdf";
+            /* Get template path */
+            var tplPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "HtmlToPdf",
+             "receipt.cshtml");
+            var tpl = System.IO.File.ReadAllText(tplPath);
+
+            /* GetOrderInfo */
+            var orderInfo = await this._orderRepository.GetOrdersListByOrderId(26, "fr"); // todo change 
+
+            var receipt = new ReceiptInfo();
+            var order = orderInfo.GetType().GetProperty("OrderInfo").GetValue(orderInfo, null);
+            if (order != null)
+            {
+                receipt.OrderId = order.Id;
+                receipt.CreatedOn = order.CreatedOn;
+                receipt.TotalPrice = order.TotalPrice;
+            }
+            var tax = orderInfo.GetType().GetProperty("TaxRate").GetValue(orderInfo, null);
+ 
+            if (tax != null)
+            {
+                receipt.Tax = receipt.TotalPrice *  double.Parse(tax.GetType().GetProperty("Value").GetValue(tax, null)) * 0.01;
+            }
+
+            var productList = orderInfo.GetType().GetProperty("ProductList").GetValue(orderInfo, null);
+
+            if (productList != null)
+            {
+                foreach (var item in productList)
+                {
+                    receipt.ProductList.Add(new ReceiptProductList() { Code = item.GetType().GetProperty("Code").GetValue(item, null),
+                                                                    Label = item.GetType().GetProperty("Label").GetValue(item, null),
+                                                                    Price = item.GetType().GetProperty("Price").GetValue(item, null),
+                                                                    Quantity  = item.GetType().GetProperty("Quantity").GetValue(item, null),
+                    });
+                }
+            }
+      
+
+
+            /* Generate pdf */
+            var exporter = new PdfExporter();
+            var result = await exporter.ExportByTemplate(fileName, receipt
+               , tpl);
+
+            //new ReceiptInfo
+            //{
+            //    Amount = 22939.43M,
+            //    Grade = "2019秋",
+            //    IdNo = "43062619890622xxxx",
+            //    Name = "张三",
+            //    Payee = "湖南心莱信息科技有限公司",
+            //    PaymentMethod = "微信支付",
+            //    Profession = "运动训练",
+            //    Remark = "学费",
+            //    TradeStatus = "已完成",
+            //    TradeTime = DateTime.Now,
+            //    UppercaseAmount = "贰万贰仟玖佰叁拾玖圆肆角叁分",
+            //    Code = "19071800001"
+            //},
+
+            /* Download file function */
+            byte[] fileBytes = System.IO.File.ReadAllBytes(fileName);
+            return File(fileBytes, "application/x-msdownload", DateTime.Now.ToString() + "_Invoice.pdf");
+
+        }
+    }
+}
---
+++ JLSMobileApplication/HtmlToPdf/ReceiptInfo.cs
@@ -0,0 +1,44 @@
+﻿using JLSDataModel.Models.Adress;
+using JLSDataModel.Models.Order;
+using Magicodes.ExporterAndImporter.Core;
+using Magicodes.ExporterAndImporter.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JLSMobileApplication.HtmlToPdf
+{
+    [PdfExporter(Name = "Facture")]
+    public class ReceiptInfo
+    {
+        public ReceiptInfo()
+        {
+            this.ShipmentAddress = new Adress();
+            this.FacturationAddress = new Adress();
+            this.ProductList = new List<ReceiptProductList>();
+        }
+        public long OrderId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Username { get; set; }
+        public string Entreprise { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public double Tax { get; set; }
+        public double TotalPrice { get; set; }
+
+        public Adress ShipmentAddress { get; set; }
+        public Adress FacturationAddress { get; set; }
+
+        public List<ReceiptProductList> ProductList { get; set; }
+    }
+
+    public class ReceiptProductList
+    {
+        public string Code { get; set; }
+        public string Label { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
