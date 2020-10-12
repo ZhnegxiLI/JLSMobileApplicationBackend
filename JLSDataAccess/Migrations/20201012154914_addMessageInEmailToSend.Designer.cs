ae45500b4dde12756bd89f2af270e3025aeb1890
ZhengxiLI <57601687+ZhnegxiLI@users.noreply.github.com>
2020-10-12 17:50:47 +0200

    Add message in emailToSend migration

--- JLSDataAccess/Migrations/20201012154914_addMessageInEmailToSend.Designer.cs
+++ JLSDataAccess/Migrations/20201012154914_addMessageInEmailToSend.Designer.cs
@@ -4,14 +4,16 @@ using JLSDataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JLSDataAccess.Migrations
 {
     [DbContext(typeof(JlsDbContext))]
-    partial class JlsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201012154914_addMessageInEmailToSend")]
+    partial class addMessageInEmailToSend
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
